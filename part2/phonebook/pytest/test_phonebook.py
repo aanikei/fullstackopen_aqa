368ddbce2ccc816c94ce0c803dbd7eb49460765c
Artur Anikeich <65artur65@gmail.com>
2024-10-27 18:00:06 +0000

    Test: Add api tests, missing UI tests and allure decorators

--- part2/phonebook/pytest/test_phonebook.py
+++ part2/phonebook/pytest/test_phonebook.py
@@ -1,26 +1,56 @@
 #pytest -s
+#pytest --alluredir allure-results
+import pytest
 import views.phonebook_app as app
+import allure
 
+test_data = [
+    {'name': 'Danny', 'phone': '1234567'},
+    {'name': 'DANTE', 'phone': '33-44-55-0'},
+]
+
+@allure.title("Verify entries")
+@allure.description("Verification that the displayed entries are the same as in data base")
+@allure.testcase("P2-Ph1")
 def test_entries_are_correct(home, db_entries):
     app.verify_page_loaded(home)
     app.verify_all_entries_loaded(home, db_entries)
 
-def test_add_and_delete_buttons(home):
+@allure.title("Verify add and delete functionality")
+@allure.description("Verification that the new entries can be added and deleted")
+@allure.feature("Add entry")
+@allure.feature("Delete entry")
+@allure.testcase("P2-Ph2")
+@pytest.mark.parametrize("entry", test_data)
+def test_add_and_delete_buttons(home, entry):
+    app.test_entries = entry
     app.verify_page_loaded(home)
     app.verify_new_entry_added(home)
-    app.verify_notification(home)
+    app.verify_added_notification(home)
     app.verify_entry_saved_in_db(home, True)
     app.verify_duplicate_not_inserted(home)
+    app.verify_update_and_cancel(home)
+    app.verify_update_and_confirm(home)
+    app.verify_updated_notification(home)
+    app.verify_entry_saved_in_db(home, True)
     app.verify_delete_and_cancel(home)
     app.verify_delete_and_confirm(home)
     app.verify_entry_saved_in_db(home, False)
     
-def test_filter_functionality(home, db_entries):
+@allure.title("Verify filter functionality")
+@allure.description("Verification that the filter works as expected")
+@allure.feature("Filter")
+@allure.testcase("P2-Ph3")
+@pytest.mark.parametrize("entry", test_data)
+def test_filter_functionality(home, db_entries, entry):
+    app.test_entries = entry
     app.verify_page_loaded(home)
     app.verify_filter(home, db_entries, 'd')
     app.verify_filter(home, db_entries, 'da')
     app.verify_filter(home, db_entries, 'dan')
+    app.verify_filter(home, db_entries, 'DAN')
     app.verify_new_entry_added(home)
-    app.verify_notification(home)
+    app.verify_added_notification(home)
     app.verify_delete_and_confirm(home)
+    app.verify_filter(home, db_entries, 'NO_SUCH_NAME')
     app.verify_filter(home, db_entries, '')
--- part2/phonebook/pytest/views/phonebook_app.py
+++ part2/phonebook/pytest/views/phonebook_app.py
@@ -2,62 +2,91 @@
 import time
 from selenium.common.exceptions import NoSuchElementException
 import pytest
+import inspect
+import allure
 
-test_entries = [
-	{'name': 'Danny', 'phone': '1234567'}
-]
+test_entries = None
 
 def verify_page_loaded(home):
-    assert "Phonebook" == home.get_app_header()
+	with allure.step(whoami()):
+		assert "Phonebook" == home.get_app_header()
     
 def verify_all_entries_loaded(home, db_entries):
-	page_entries = []
-	for e in home.get_entries():
-		page_entries.append(e.text.removesuffix('delete'))
+	with allure.step(whoami()):
+		page_entries = []
+		for e in home.get_entries():
+			page_entries.append(e.text.removesuffix('delete'))
 
-	assert db_entries == page_entries
+		assert db_entries == page_entries
 
 def verify_new_entry_added(home):
-	home.add_new_entry(test_entries[0]['name'], test_entries[0]['phone'])
+	with allure.step(whoami()):
+		home.add_new_entry(test_entries['name'], test_entries['phone'])
+		# time.sleep(0.1) # reduce flakiness
+		new_entry = (home.get_entries()[-1]).text.removesuffix('delete')
+		assert f'{test_entries['name']} {test_entries['phone']}' == new_entry
 
-	new_entry = (home.get_entries()[-1]).text.removesuffix('delete')
-	assert f'{test_entries[0]['name']} {test_entries[0]['phone']}' == new_entry
-
-def verify_notification(home):
+def verify_notification(home, action: str):
 	message = home.get_success_message()
-	assert message == f"Added '{test_entries[0]['name']}'"
+	assert message == f"{action} '{test_entries['name']}'"
 	time.sleep(5)
 	with pytest.raises(NoSuchElementException):
 		home.get_success_message()
 
-def verify_entry_saved_in_db(home, is_saved: bool):
-	home.refresh_page()
+def verify_added_notification(home):
+	with allure.step(whoami()):
+		verify_notification(home, 'Added')
 
-	new_entry = (home.get_entries()[-1]).text.removesuffix('delete')
-	if is_saved:
-		assert f'{test_entries[0]['name']} {test_entries[0]['phone']}' == new_entry
-	else:
-		assert f'{test_entries[0]['name']} {test_entries[0]['phone']}' != new_entry
+def verify_updated_notification(home):
+	with allure.step(whoami()):
+		verify_notification(home, 'Updated')
+
+def verify_entry_saved_in_db(home, is_saved: bool):
+	with allure.step(whoami()):
+		home.refresh_page()
+
+		new_entry = (home.get_entries()[-1]).text.removesuffix('delete')
+		if is_saved:
+			assert f'{test_entries['name']} {test_entries['phone']}' == new_entry
+		else:
+			assert f'{test_entries['name']} {test_entries['phone']}' != new_entry
 
 def verify_duplicate_not_inserted(home):
-	num_entries_before = len(home.get_entries())
-	home.add_new_entry(test_entries[0]['name'], test_entries[0]['phone'])
-	alert_text = home.handle_duplicate_alert()
-	assert num_entries_before == len(home.get_entries())
-	assert alert_text == f'{test_entries[0]['name']} is already added to phonebook!'
+	with allure.step(whoami()):
+		num_entries_before = len(home.get_entries())
+		home.add_new_entry(test_entries['name'], test_entries['phone'])
+		alert_text = home.handle_duplicate_alert()
+		assert num_entries_before == len(home.get_entries())
+		assert alert_text == f'{test_entries['name']} is already added to phonebook!'
 
 def verify_delete_and_cancel(home):
-	home.delete_last_entry(False)
-	last_entry = (home.get_entries()[-1]).text.removesuffix('delete')
-	assert f'{test_entries[0]['name']} {test_entries[0]['phone']}' == last_entry
+	with allure.step(whoami()):
+		home.delete_last_entry(False)
+		last_entry = (home.get_entries()[-1]).text.removesuffix('delete')
+		assert f'{test_entries['name']} {test_entries['phone']}' == last_entry
 
 def verify_delete_and_confirm(home):
-	home.delete_last_entry(True)
-	time.sleep(0.1) #fix StaleElementReferenceException: Message: stale element reference: stale element not found in the current frame
-	last_entry = (home.get_entries()[-1]).text.removesuffix('delete')
-	assert f'{test_entries[0]['name']} {test_entries[0]['phone']}' != last_entry
+	with allure.step(whoami()):
+		home.delete_last_entry(True)
+		time.sleep(0.1) #fix StaleElementReferenceException: Message: stale element reference: stale element not found in the current frame
+		last_entry = (home.get_entries()[-1]).text.removesuffix('delete')
+		assert f'{test_entries['name']} {test_entries['phone']}' != last_entry
 
 def verify_filter(home, db_entries, s: str):
-	filtered_list = home.filter_entries(s)
-	filtered_db = [i for i in db_entries if s in i.lower()]
-	assert filtered_list == filtered_db
+	with allure.step(whoami()):
+		filtered_list = home.filter_entries(s)
+		filtered_db = [i for i in db_entries if s.lower() in i.lower()]
+		assert filtered_list == filtered_db
+
+def verify_update_and_cancel(home):
+	with allure.step(whoami()):
+		test_entries['phone'] = '098765'
+		home.update_entry(test_entries['name'], test_entries['phone'], False)
+
+def verify_update_and_confirm(home):
+	with allure.step(whoami()):
+		home.update_entry(test_entries['name'], test_entries['phone'], True)
+
+
+def whoami():
+	return inspect.stack()[1][3].replace('_', ' ')
